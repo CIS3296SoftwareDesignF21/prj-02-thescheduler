c4e9bfb2a7b477de8e76cba4ab6738a7ce838448
Felix Rabinovich <tuf58324@temple.edu>
2021-10-27 12:36:07 -0400

    Added HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, this is the main page of the soon to be SCHEDULER !!" \
+           "<h1>Hello World<h1>" \
+           "<p>Hello this is Felix" \
+           "<br>Hello this is ____" \
+           "<br>Hello this is ____" \
+           "<br>Hello this is ____" \
+           "<br>Hello this is ____" \
+           "<br>Hello this is ____<p>"
+
+if __name__ == "__main__":
+    app.run()
