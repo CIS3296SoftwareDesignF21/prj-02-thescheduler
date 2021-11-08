db601a5700e1fb2c8c08ac48f33a4ad88a0b4f2e
tuh00755 <tuh00755@temple.edu>
2021-11-08 14:19:46 -0500

    updated input.py

--- input.py
+++ input.py
@@ -1,21 +1,49 @@
 from selenium import webdriver
 import time
 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+fo = open('input_text.txt' 'w')
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
-driver.get("http://127.0.0.1:5000/") ## we dont have an url for the website yet so ...
+#### Code to run webside
 
-input = open('input.txt', 'w')
-time.sleep(10)
+driver.get("https://thescheduler.pythonanywhere.com/course_select%22")
 
-course_dept = driver.find_element_by_id("fdept")
-search = driver.find_element_by_name("fcrs")
+department = driver.find_element_by_id("fdept")
+crs = department
 
-print(search, course_dept, file = input)
+course_number = driver.find_element_by_id("fcrs")
+crn = course_number
 
+driver.get("https://prd-xereg.temple.edu/StudentRegistrationSsb/ssb/registration/registration%22")
 
-input.close()
+schedule = driver.find_element_by_id('catalogSearchLink')
+schedule.click
+
+select = driver.find_element_by_id("select2-drop-mask")
+select.send_keys("2022 spring")
+
+term = driver.find_element_by_id("term-go")
+term.click
+
+subject = driver.find_element_by_id("select2-container")
+subject.send_keys(crs)
+
+crnum = driver.find_element_by_name("txt_course_number_range_From")
+crnum.send_keys(crn)
+
+search = driver.find_element_by_id("search-go")
+search.click
+
+time.sleep(10) # waits 5 seconds
+
+print(crs, crn, file = fo )
+driver.quit()
+fo.close()
 
 
 
