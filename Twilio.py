35d19b0ff8d61ebbf46dfab386e05aa88559a734
Felix Rabinovich <tuf58324@temple.edu>
2021-11-10 14:55:05 -0500

    Update Twilio.py

--- Twilio.py
+++ Twilio.py
@@ -3,13 +3,13 @@ from twilio.rest import Client
 # Your Account SID from twilio.com/console
 account_sid = "ACbf02fe253cef5d92aac22aa3bd5b1676"
 # Your Auth Token from twilio.com/console
-auth_token = ""
+auth_token = ""  # Get from discord, Twilio blocks functionality if this token is in a rebo
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
     to="+12158079223",
     from_="+12156087254",
-    body="Hello from Python!")
+    body="Cool, this works and we can now notify our clientele via SMS")
 
 print(message.sid)
