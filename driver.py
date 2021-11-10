1f24c29f52d2a216491e5ab4f7420de8ade8137c
Felix Rabinovich <tuf58324@temple.edu>
2021-11-10 14:14:10 -0500

    Twilio Rework
    Need to rework Twilio, so I removed it

--- driver.py
+++ driver.py
@@ -1,6 +1,4 @@
 from flask import Flask, redirect, url_for, render_template, request
-import main
-
 app = Flask(__name__)
 
 """
@@ -8,6 +6,7 @@ Used the following as a guide:
 https://www.techwithtim.net/tutorials/flask/http-methods-get-post/
 """
 
+#import main
 
 @app.route("/")
 def home():
@@ -19,7 +18,7 @@ def course_select():
     if request.method == "POST":
         dept = request.form["fdept"]
         crs = request.form["fcrs"]
-        #main() intergratted but has bugs
+        #main() #intergratted but has bugs
         return redirect(url_for("post_course", department=dept, course_number=crs))
     else:
         return render_template("index.html")
