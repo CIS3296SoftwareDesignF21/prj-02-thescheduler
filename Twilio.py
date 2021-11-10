7084f8dc4e4bf6a7e45c4163ba445953fbb82c7b
Felix Rabinovich <tuf58324@temple.edu>
2021-11-10 14:48:37 -0500

    Update Twilio.py

--- Twilio.py
+++ Twilio.py
@@ -3,7 +3,7 @@ from twilio.rest import Client
 # Your Account SID from twilio.com/console
 account_sid = "ACbf02fe253cef5d92aac22aa3bd5b1676"
 # Your Auth Token from twilio.com/console
-auth_token = "01dad8c9bf80cb7ae9a2489664c6bca6"
+auth_token = ""
 
 client = Client(account_sid, auth_token)
 
