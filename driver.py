c14d62adc01a09b800fbea28fcaf8e0db9c48e2a
Felix Rabinovich <tuf58324@temple.edu>
2021-11-14 20:38:25 -0500

    update Driver.py
    Driver.py now takes more than one input

--- driver.py
+++ driver.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, url_for, render_template, request
+
 app = Flask(__name__)
 
 """
@@ -6,7 +7,8 @@ Used the following as a guide:
 https://www.techwithtim.net/tutorials/flask/http-methods-get-post/
 """
 
-#import main
+
+# import main
 
 @app.route("/")
 def home():
@@ -17,16 +19,24 @@ def home():
 def course_select():
     if request.method == "POST":
         dept = request.form["fdept"]
-        crs = request.form["fcrs"]
-        #main() #intergratted but has bugs
-        return redirect(url_for("post_course", department=dept, course_number=crs))
+        crs1 = request.form["fcrs1"]
+        crs2 = request.form["fcrs2"]
+        crs3 = request.form["fcrs3"]
+        crs4 = request.form["fcrs4"]
+        # main() #intergratted but has bugs
+        return redirect(
+            url_for("post_course", department=dept, course_number1=crs1, course_number2=crs2, course_number3=crs3,
+                    course_number4=crs4))
     else:
         return render_template("index.html")
 
 
-@app.route("/<department>&<course_number>")
-def post_course(department, course_number):
-    return f"<h1>Go CST!! You're taking:{department},{course_number}</h1>"
+@app.route("/<department>&<course_number1>&<course_number2>&<course_number3>&<course_number4>")
+def post_course(department, course_number1, course_number2, course_number3, course_number4):
+    return f"<h1>Go CST!! You're taking:{department},{course_number1}</h1>" \
+           f"<h1>Go CST!! You're taking:{department},{course_number2}</h1>" \
+           f"<h1>Go CST!! You're taking:{department},{course_number3}</h1>" \
+           f"<h1>Go CST!! You're taking:{department},{course_number4}</h1>"
 
 
 if __name__ == "__main__":
