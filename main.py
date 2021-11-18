a1732f649bba1db950ba819da78d35549751e4e1
Felix Rabinovich <tuf58324@temple.edu>
2021-11-18 09:27:46 -0500

    updated puzzle
    Selenium is now integrated with main woop woop

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from flask import Flask, redirect, url_for, render_template, request
 
+
 app = Flask(__name__)
 
 """
 Used the following as a guide:
 https://www.techwithtim.net/tutorials/flask/http-methods-get-post/
 """
-
 
 
 
@@ -23,7 +23,7 @@ def course_select():
         crs2 = request.form["fcrs2"]
         crs3 = request.form["fcrs3"]
         crs4 = request.form["fcrs4"]
-        # main() #integrated but has bugs
+
         return redirect(
             url_for("post_course", department=dept, course_number1=crs1, course_number2=crs2, course_number3=crs3,
                     course_number4=crs4))
@@ -31,16 +31,11 @@ def course_select():
         return render_template("index.html")
 
 
-@app.route("/<department>&<course_number1>&<course_number2>&<course_number3>&<course_number4>")
-def post_course(department, course_number1, course_number2, course_number3, course_number4):
-    return f"<h1>Go CST!! You're taking:{department},{course_number1}</h1>" \
-           f"<h1>Go CST!! You're taking:{department},{course_number2}</h1>" \
-           f"<h1>Go CST!! You're taking:{department},{course_number3}</h1>" \
-           f"<h1>Go CST!! You're taking:{department},{course_number4}</h1>"
+@app.route("/<department>&<course_number1>")
+def post_course(department, course_number1):
+    import selen
+    selen(department=department, course=course_number1)
 
-def run_hub():
-    import hub
-    hub()
-  
+
 if __name__ == "__main__":
     app.run(debug=True)
--- selen.py
+++ selen.py
@@ -4,24 +4,15 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
-
 import time
+
+department = ''
+course = ''
 
 PATH = "/usr/local/bin/chromedriver"
 #PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
-#### Code to run webside
-"""
-driver.get("https://thescheduler.pythonanywhere.com/course_select")
-
-time.sleep(2)
-department = driver.find_element_by_id("fdept")
-crs = department
-
-course_number = driver.find_element_by_id("fcrs")
-crn = course_number
-"""
 driver.get("https://prd-xereg.temple.edu/StudentRegistrationSsb/ssb/term/termSelection?mode=courseSearch")
 #driver.get("https://prd-xereg.temple.edu/StudentRegistrationSsb/ssb/courseSearch/courseSearch")
 
@@ -54,25 +45,18 @@ subject_before.click()
 
 
 subject_after = driver.find_element_by_id("s2id_autogen1")
-subject_after.send_keys('CIS')
+subject_after.send_keys(department)
 time.sleep(1)
 subject_after.send_keys(Keys.RETURN)
-## works till here
 
-
-'''
-time.sleep(2)
-crnum = driver.find_element_by_name("txt_courseNumber")
-crnum.send_keys('3207')
-'''
 
 crnum_from = driver.find_element_by_name("txt_course_number_range_From")
 crnum_from.click()
-crnum_from.send_keys('3207')
+crnum_from.send_keys(course)
 
 
 crnum_to = driver.find_element_by_name("txt_course_number_range_To")
-crnum_to.send_keys('3207')
+crnum_to.send_keys(course)
 
 
 search = driver.find_element_by_id("search-go")
@@ -82,81 +66,6 @@ time.sleep(1)
 view_sections = driver.find_element_by_class_name("form-button.search-section-button")
 view_sections.click()
 
-'''
-search_again = driver.find_element_by_id("search-again-button")
-search_again.click()   Needed for loop 
-
-CourseTitle = driver.find_element_by_id("courseTitle")
-print(CourseTitle)    This print is to check if the scrapper is working
-
-section_number = driver.find_element_by_id("sectionNumber")
-print(section_number)
-
-credit_hours = driver.find_elements_by_name("Credit Hours:")
-print(credit_hours)
-
-'''
-
-
 time.sleep(10) # waits 5 seconds
 
 driver.quit()
-'''
-# Dictionary storing for course info
-Schedule = {1: {'Title': '', 'Time': '', 'Instructor': '', 'Credits': ''},
-            2: {'Title': '', 'Time': '', 'Instructor': '', 'Credits': ''},
-            3: {'Title': '', 'Time': '', 'Instructor': '', 'Credits': ''},
-            4: {'Title': '', 'Time': '', 'Instructor': '', 'Credits': ''}}
-
-classQueue is a queue implemented using deque() who's elements consist of courses.
-Classes are dict entries structured the following way: 
-
-{'Subject': '', 'Course Number': ''}
-
-Example: {'Subject': 'Computer & Information Science', 'Course Number': '3296'}
-
-
-###
-!! Browse Courses page must be navigated to prior to loop !!
-
-select = driver.find_element_by_class_name("select2-drop")
-select.send_keys("2022 Spring")
-
-term = driver.find_element_by_id("term-go")
-term.click
-
-def parseQueue(courseQueue):
-    while courseQueue:
-        currentClass = courseQueue.pop() #Store the next entry in memory, delete it from the queue
-        currentSubject = currentClass['Subject']
-        currentCourseNum = currentClass['Course Number']
-
-
-
-        subject = driver.find_element_by_id("select2-container")
-        subject.send_keys(currentSubject)
-
-        crnum = driver.find_element_by_name("txt_course_number_range_From")
-        crnum.send_keys(currentCourseNum)
-
-        search = driver.find_element_by_id("search-go")
-        search.click
-
-        # Experimental from here till end of loop
-        crsSelect = driver.find_element_by_class_name("form-button search-section-button")
-        crsSelect.click
-
-        #callCourseScraper
-
-        backToSearch = driver.find_element_by_class_name("form-button return-course-button")
-        backToSearch.click
-
-        searchAgain = driver.find_element_by_id("search-again-button")
-        searchAgain.click
-
-
-    return
-# print(crs, crn, file = fo )
-driver.quit()
-# fo.close()
-'''
