f46fd8ec19c0a58fe63e168a76c5f0ff7a4fdccf
Felix Rabinovich <tuf58324@temple.edu>
2021-11-23 15:34:07 -0500

    Update Twilio.py

--- Twilio.py
+++ Twilio.py
@@ -1,14 +1,14 @@
 from twilio.rest import Client
 
 TWILIO_SID = 'ACbf02fe253cef5d92aac22aa3bd5b1676'
-TWILIO_TOKEN = '01dad8c9bf80cb7ae9a2489664c6bca6'
+TWILIO_TOKEN = ''
 TWILIO_PHONE = '+12156087254'
 
 client = Client(TWILIO_SID, TWILIO_TOKEN)
 
 
 def sendOneMessage(sendTo):
-    client.messages.create(body="Our first SMS message from twillio", from_=TWILIO_PHONE, to=sendTo)
+    client.messages.create(body="Hey There ! Your schedule is complete, you can take a look at it on our site !", from_=TWILIO_PHONE, to=sendTo)
 
 sendOneMessage('+12158079223')
 
