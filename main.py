7e07fa1c5a3d993672e47562e866b3e5d0817006
Felix Rabinovich <tuf58324@temple.edu>
2021-11-23 11:08:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -36,55 +36,50 @@ def course_select():
 
 @app.route("/<department>&<course_number1>")
 def get_course(department, course_number1):
-    #PATH = "/usr/local/bin/chromedriver"
+    file = open("scraped_details.txt", "w")
+    # PATH = "/usr/local/bin/chromedriver"
     PATH = "C:\Program Files (x86)\chromedriver.exe"
     driver = webdriver.Chrome(PATH)
 
     driver.get("https://prd-xereg.temple.edu/StudentRegistrationSsb/ssb/term/termSelection?mode=courseSearch")
-    #driver.get("https://prd-xereg.temple.edu/StudentRegistrationSsb/ssb/courseSearch/courseSearch")
-
+    # driver.get("https://prd-xereg.temple.edu/StudentRegistrationSsb/ssb/courseSearch/courseSearch")
 
     sel = driver.find_element_by_id('s2id_txt_term')
     sel.click()
 
-    #sel.select_by_visible_text("Spring 2022")
+    # sel.select_by_visible_text("Spring 2022")
     time.sleep(2)
 
     select = driver.find_element_by_id("s2id_autogen1_search")
     select.send_keys('2022 Spring')
     time.sleep(1)
     select.click()
-    #select.select_by_value('2022 Spring')
+    # select.select_by_value('2022 Spring')
 
     time.sleep(1)
     spring = driver.find_element_by_id("select2-results-1")
     spring.click()
-    #spring.select_by_visible_text('2022 Spring')
+    # spring.select_by_visible_text('2022 Spring')
 
     submit = driver.find_element_by_id('term-go')
     submit.click()
-
 
     time.sleep(1)
 
     subject_before = driver.find_element_by_id("s2id_txt_subject")
     subject_before.click()
 
-
     subject_after = driver.find_element_by_id("s2id_autogen1")
     subject_after.send_keys(department)
     time.sleep(1)
     subject_after.send_keys(Keys.RETURN)
 
-
     crnum_from = driver.find_element_by_name("txt_course_number_range_From")
     crnum_from.click()
     crnum_from.send_keys(course_number1)
 
-
     crnum_to = driver.find_element_by_name("txt_course_number_range_To")
     crnum_to.send_keys(course_number1)
-
 
     search = driver.find_element_by_id("search-go")
     search.click()
@@ -93,9 +88,30 @@ def get_course(department, course_number1):
     view_sections = driver.find_element_by_class_name("form-button.search-section-button")
     view_sections.click()
 
+    #######################################################################
+    time.sleep(1)
 
+    before_course_number = driver.find_element_by_class_name("odd")
+    before_course_number.click()
 
-    time.sleep(10) # waits 5 seconds
+    time.sleep(3)
+    course_number = driver.find_elements_by_class_name('odd')
+    # digit = course_number.text
+    for element in course_number:
+        # print (course_number.text)
+        # print(driver.find_element_by_class_name("readonly.add-row-selected").text)
+        print(element.text, file=file)
+
+    time.sleep(2)
+    course_number = driver.find_elements_by_class_name('even')
+    # digit = course_number.text
+    for element in course_number:
+        # print (course_number.text)
+        # print(driver.find_element_by_class_name("readonly.add-row-selected").text)
+        print(element.text, file=file)
+
+    time.sleep(10)  # waits 10 seconds
+    file.close()
 
     driver.quit()
 
