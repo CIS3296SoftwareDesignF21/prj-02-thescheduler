77ed45c232741d6b713171a2053e8fa9d6eb8b30
Felix Rabinovich <tuf58324@temple.edu>
2021-11-24 14:25:52 -0500

    moved twilio in to main

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
+from twilio.rest import Client
 import time
 app = Flask(__name__)
 
@@ -174,6 +175,21 @@ def get_course(department, course_number1):
     driver.quit()
     # fo.close()
     '''
+
+def twilio():
+    TWILIO_SID = 'ACbf02fe253cef5d92aac22aa3bd5b1676'
+    TWILIO_TOKEN = ''
+    TWILIO_PHONE = '+12156087254'
+
+    client = Client(TWILIO_SID, TWILIO_TOKEN)
+
+    def sendOneMessage(sendTo):
+        client.messages.create(body="Hey There ! Your schedule is complete, you can take a look at it on our site !",
+                               from_=TWILIO_PHONE, to=sendTo)
+
+    sendOneMessage('+12158079223')
+
+    print('SMS sent succesfully')
 
 if __name__ == "__main__":
     app.run(debug=True)
