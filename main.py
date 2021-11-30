6fcc7b61cf2d8bd9be2749b0cb3844113b75adcb
Felix Rabinovich <tuf58324@temple.edu>
2021-11-30 13:41:33 -0500

    updated

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, url_for, render_template, request
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,7 +11,7 @@ import time
 import requests
 import lxml.html as lh
 
-import algorithm
+
 
 app = Flask(__name__)
 
@@ -53,9 +54,28 @@ def get_course(department, courses_input):
             print("No course given")
         else:
             file = open("scraped_details.txt", "w")
+            user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
+            #PATH = os.path.join(sys.path[0], "chromedriver")
             PATH = "/usr/local/bin/chromedriver"
             #PATH = "C:\Program Files (x86)\chromedriver.exe"
-            driver = webdriver.Chrome(PATH)
+
+            chrome_options = Options()
+            chrome_options.headless = True
+            chrome_options.add_argument(f'user-agent={user_agent}')
+            chrome_options.add_argument("--window-size=1920,1080")
+            chrome_options.add_argument('--ignore-certificate-errors')
+            chrome_options.add_argument('--allow-running-insecure-content')
+            chrome_options.add_argument("--disable-extensions")
+            chrome_options.add_argument("--proxy-server='direct://'")
+            chrome_options.add_argument("--proxy-bypass-list=*")
+            chrome_options.add_argument("--start-maximized")
+            chrome_options.add_argument('--disable-gpu')
+            chrome_options.add_argument('--disable-dev-shm-usage')
+            chrome_options.add_argument('--no-sandbox')
+
+            driver = webdriver.Chrome(PATH, options = chrome_options)
+            #driver = webdriver.Chrome(PATH)
+
             # page = requests.get("https://prd-xereg.temple.edu/StudentRegistrationSsb/ssb/term/termSelection?mode=courseSearch")
             driver.get("https://prd-xereg.temple.edu/StudentRegistrationSsb/ssb/term/termSelection?mode=courseSearch")
             # driver.get("https://prd-xereg.temple.edu/StudentRegistrationSsb/ssb/courseSearch/courseSearch")
